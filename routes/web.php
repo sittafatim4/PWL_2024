5a1f61426fe5eaf97c4e2cb306288f1aa62d6c41
sittafatim4 <sitta.fatimaa@gmail.com>
2025-02-18 14:26:38 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+   return 'Hello World';
+});
+
+Route::get('/world', function () {
+   return 'World';
+});
+
+Route::get('/selamatDatang', function () {
+   return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2341760167 <br> NAMA : FATIMA SITTA MAULIDIA';
+ });
+
+ Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});    
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles{id}', function ($articlesId) {
+    return 'Id ke-'.$articlesId;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});    
+    
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +44,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
