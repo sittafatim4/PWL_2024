1f5e53624feacdacc25b525ef4fe9579441f6cd9
sittafatim4 <sitta.fatimaa@gmail.com>
2025-05-05 13:15:41 +0700

    update controller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\WelcomeController;
 
 use App\Http\Controllers\PageController;
 
+use App\Http\Controllers\LevelController;
+
 use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\AboutController;
@@ -13,6 +15,12 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 
 use App\Http\Controllers\PhotoController;
+
+Route::get('/', function() {
+    return view('welcome');
+});
+
+Route::get('/level', [levelController::class, 'index']);
 
 //Route::get('/greeting', function () {
 	//return view('blog.hello', ['name' => 'Sitta']);
